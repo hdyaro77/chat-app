2ac1ab53416601a9321cf44fb1fc8088e048758d
hdyaro77 <harshit.dubey1923@gmail.com>
2022-06-27 19:03:20 +0530

    fixed security issue on git

--- backend/Controllers/upload_handler.js
+++ backend/Controllers/upload_handler.js
@@ -1,6 +1,6 @@
 const util = require("util");
 const { GridFsStorage } = require("multer-gridfs-storage");
-const DB = "mongodb+srv://harshit:Harshit%40123@cluster0.0hhe5.mongodb.net/chat_app";
+const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.PASSWORD);
 const multer = require("multer");
 const storage = new GridFsStorage({
   url: DB,
